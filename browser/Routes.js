d64b6ac0d31cd6613414ff26cb8272e449914c9f
Tom Schuster <tomjschuster@gmail.com>
2016-11-14 11:33:14 -0500

    add ace/stage-2

--- browser/Routes.js
+++ browser/Routes.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import { Router, Route, browserHistory, IndexRoute, Redirect } from 'react-router';
 import App from './components/App';
 import CreateModel from './components/CreateModel';
+import Ace from './components/Ace';
 
 export default class Routes extends Component {
   render() {
     return (
     <Router history={browserHistory}>
       <Route path="/" component={App}>
+        <Route path="ace" component={Ace}/>
         <IndexRoute component={CreateModel}/>
         <Redirect from="*" to="/"/>
       </Route>
--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel',
         query: {
-          presets: ['react', 'es2015']
+          presets: ['react', 'es2015', 'stage-2']
         }
       }
     ]
