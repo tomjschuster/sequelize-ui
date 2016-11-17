a16890ab5881c23ab6e2ae281eb50ab0cc27fb4d
Tom Schuster <tomjschuster@gmail.com>
2016-11-17 00:59:51 -0500

    remove react tap touch plugin

--- browser/index.js
+++ browser/index.js
@@ -1,11 +1,5 @@
 'use strict';
 
-import injectTapEventPlugin from 'react-tap-event-plugin';
-
-// Needed for onTouchTap
-// http://stackoverflow.com/a/34015469/988941
-injectTapEventPlugin();
-
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
