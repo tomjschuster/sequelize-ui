9627e8b8036170d276326c37042d957724f8b097
Tom Schuster <tomjschuster@gmail.com>
2017-05-29 01:08:08 -0400

    install post-css and add to webpack config

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -37,7 +37,11 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        use: [
+          'style-loader',
+          { loader: 'css-loader', options: { importLoaders: 1 } },
+          'postcss-loader'
+        ]
       }
     ]
   }
