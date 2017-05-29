3247360f5a8338f6e87639bd479261edc704f179
Tom Schuster <tomjschuster@gmail.com>
2017-05-29 00:59:23 -0400

    implement css loader

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
+import css from '../../assets/style.css'
 
 export default class App extends Component {
   render () {
     return (
-      <div>
+      <div className='test-class'>
         <h1>Sequelize UI</h1>
       </div>
     )
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -34,6 +34,10 @@ module.exports = {
         exclude: path.join(__dirname, 'node_modules'),
         include: path.join(__dirname, 'src'),
         use: ['babel-loader']
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
       }
     ]
   }
