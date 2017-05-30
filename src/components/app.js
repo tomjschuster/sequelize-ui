66954de4bf681e6ca28525f4e882610373e9bd0f
Tom Schuster <tomjschuster@gmail.com>
2017-05-30 01:19:29 -0400

    add avatar and headers for all test components

--- src/components/app.js
+++ src/components/app.js
@@ -3,6 +3,7 @@ import styles from '../../assets/style.css'
 import AppBar from 'react-toolbox/lib/app_bar'
 import { Layout, Panel } from 'react-toolbox'
 import Autocomplete from 'react-toolbox/lib/autocomplete'
+import Avatar from 'react-toolbox/lib/avatar'
 
 const source = {
   'ES-es': 'Spain',
@@ -17,7 +18,7 @@ export default class App extends Component {
   }
 
   handleChange = (value) => {
-    this.setState({countries: value});
+    this.setState({ countries: value });
   }
 
   render () {
@@ -25,15 +26,42 @@ export default class App extends Component {
       <Layout>
         <Panel>
           <AppBar title='Sequelize UI' />
-            <Autocomplete
-              direction="down"
-              selectedPosition="above"
-              label="Choose countries"
-              onChange={this.handleChange}
-              source={source}
-              value={this.state.countries}
-            />
-
+            <div style={{ padding: '2%' }}>
+              <h2>Autocomplete</h2>
+              <Autocomplete
+                direction='down'
+                selectedPosition='above'
+                label='Choose countries'
+                onChange={this.handleChange}
+                source={source}
+                value={this.state.countries}
+              />
+              <h2>Avatar</h2>
+              <Avatar
+                title='folder'
+                icon='folder'
+              />
+              <h2>Button</h2>
+              <h2>Card</h2>
+              <h2>Chip</h2>
+              <h2>Checkbox</h2>
+              <h2>Date Picker</h2>
+              <h2>Dialog</h2>
+              <h2>Dropdown</h2>
+              <h2>Font Icon</h2>
+              <h2>Input</h2>
+              <h2>Link</h2>
+              <h2>List</h2>
+              <h2>Menu</h2>
+              <h2>Navigation</h2>
+              <h2>Progress Bar</h2>
+              <h2>Radio Buttons</h2>
+              <h2>Slider</h2>
+              <h2>Snackbar</h2>
+              <h2>Switch</h2>
+              <h2>Tabs</h2>
+              <h2>Tooltip</h2>
+            </div>
         </Panel>
       </Layout>
     )
