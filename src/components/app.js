d288a774c5b3caacbb8c7b94ba8b6d9af45924ba
Tom Schuster <tomjschuster@gmail.com>
2017-05-30 01:02:36 -0400

    add babel class tranform pluginand add autocomplete component

--- src/components/app.js
+++ src/components/app.js
@@ -1,16 +1,41 @@
 import React, { Component } from 'react'
 import styles from '../../assets/style.css'
 import AppBar from 'react-toolbox/lib/app_bar'
+import { Layout, Panel } from 'react-toolbox'
+import Autocomplete from 'react-toolbox/lib/autocomplete'
+
+const source = {
+  'ES-es': 'Spain',
+  'TH-th': 'Thailand',
+  'EN-gb': 'England',
+  'EN-en': 'USA'
+}
 
 export default class App extends Component {
+  state = {
+    countries: ['ES-es', 'TH-th']
+  }
+
+  handleChange = (value) => {
+    this.setState({countries: value});
+  }
+
   render () {
     return (
-      <div className={styles.testClass}>
-        <AppBar title='Sequelize UI' />
-        <p className={styles.primaryColor}>
-          The primary color  
-        </p>
-      </div>
+      <Layout>
+        <Panel>
+          <AppBar title='Sequelize UI' />
+            <Autocomplete
+              direction="down"
+              selectedPosition="above"
+              label="Choose countries"
+              onChange={this.handleChange}
+              source={source}
+              value={this.state.countries}
+            />
+
+        </Panel>
+      </Layout>
     )
   }
 }
