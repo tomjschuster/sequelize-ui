a1e5c0a3d5a766da0352dfd4991d23dfcd371184
Tom Schuster <tomjschuster@gmail.com>
2017-06-24 01:19:20 -0400

    debugging redux setup

--- app/index.js
+++ app/index.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import store from './redux'
 import ThemeProvider from 'react-toolbox/lib/ThemeProvider'
 import { theme } from 'theme'
 import App from './components/app'
 
 render(
-  <ThemeProvider theme={theme}>
-    <App />
-  </ThemeProvider>,
+  <Provider store={store}>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </Provider>,
   document.getElementById('app')
-  )
+)
 
