ee262296ee9719e9519e07044141eb0fcdae17ee
Tom Schuster <tomjschuster@gmail.com>
2017-06-25 23:49:41 -0400

    mapAction creators and fix hot-reload middleware on server

--- app/router/index.js
+++ app/router/index.js
@@ -1,6 +1,5 @@
 import createHistory from 'history/createBrowserHistory'
 import resolve from './resolve'
-console.log(resolve)
 export routes from './routes'
 export Link from './link'
 export const history = createHistory()
