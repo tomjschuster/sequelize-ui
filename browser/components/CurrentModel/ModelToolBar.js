666187ba6847cef5c8bdbb60234d0d32092ad956
Tom Schuster <tomjschuster@gmail.com>
2018-02-11 12:31:23 -0500

    toolbar to react toolbox

--- browser/components/CurrentModel/ModelToolBar.js
+++ browser/components/CurrentModel/ModelToolBar.js
@@ -1,62 +1,50 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { connect } from 'react-redux'
 
 import { saveModel, removeModel } from '../../redux/models'
 import { resetModel, setModelName } from '../../redux/currentModel'
+import Input from 'react-toolbox/lib/input'
+import { Button } from 'react-toolbox/lib/button'
 
-import TextField from 'material-ui/TextField'
-import RaisedButton from 'material-ui/RaisedButton'
-import { Toolbar, ToolbarGroup, ToolbarSeparator } from 'material-ui/Toolbar'
-import { red400, white, blueGrey200 } from 'material-ui/styles/colors'
-
-export class ModelToolBar extends Component {
-  render() {
-    let { currentModel, updateModelName, saveModel, deleteModel } = this.props
-    return (
-      <Toolbar>
-        <ToolbarGroup firstChild={true}>
-          <ToolbarSeparator />
-          <div className="model-name-input">
-            <TextField
-              value={currentModel.name}
-              style={{
-                fontSize: '1.5em'
-              }}
-              onChange={evt => updateModelName(evt.target.value)}
-              hintText="Model Name"
-              hintStyle={{ color: '#555' }}
-            />
-          </div>
-          <ToolbarSeparator />
-          {currentModel.id && (
-            <RaisedButton
-              label="Save"
-              primary={true}
-              onClick={() => saveModel(currentModel, false)}
-            />
-          )}
-          {currentModel.id && (
-            <RaisedButton
-              label="Delete"
-              labelColor={white}
-              backgroundColor={red400}
-              onClick={() => deleteModel(currentModel)}
-            />
-          )}
-          {!currentModel.id && (
-            <RaisedButton
-              label="Create"
-              disabled={!currentModel.name}
-              disabledBackgroundColor={blueGrey200}
-              secondary={true}
-              onClick={() => saveModel(currentModel, true)}
-            />
-          )}
-        </ToolbarGroup>
-      </Toolbar>
-    )
-  }
-}
+const ModelToolBar = ({
+  currentModel,
+  updateModelName,
+  saveModel,
+  deleteModel
+}) => (
+  <div>
+    <Input
+      value={currentModel.name}
+      onChange={value => updateModelName(value)}
+      hint="Model Name"
+    />
+    {currentModel.id && (
+      <Button
+        label="Save"
+        primary
+        raised
+        onClick={() => saveModel(currentModel, false)}
+      />
+    )}
+    {currentModel.id && (
+      <Button
+        label="Delete"
+        raised
+        accent
+        onClick={() => deleteModel(currentModel)}
+      />
+    )}
+    {!currentModel.id && (
+      <Button
+        label="Create"
+        disabled={!currentModel.name}
+        primary
+        raised
+        onClick={() => saveModel(currentModel, true)}
+      />
+    )}
+  </div>
+)
 
 const mapStateToProps = ({ currentModel }) => ({ currentModel })
 const mapDispatchToProps = dispatch => ({
