f543d81b20b0fc103147d3fd2d5d7b2b64d5f29f
Tom Schuster <tomjschuster@gmail.com>
2018-02-11 00:03:02 -0500

    current model tabs to react toolbox

--- browser/components/CurrentModel/CurrentModel.js
+++ browser/components/CurrentModel/CurrentModel.js
@@ -7,62 +7,42 @@ import ModelToolBar from './ModelToolBar'
 import Fields from './Fields'
 import Configuration from './Configuration'
 import Associations from './Associations'
-import Paper from 'material-ui/Paper'
 
-import {Tabs, Tab} from 'material-ui/Tabs'
-
+import { Tab, Tabs } from 'react-toolbox'
 
 export class CurrentModel extends Component {
   constructor(props) {
     super(props)
     this.state = { tabIdx: 0 }
-    this.setTabIdx = this.setTabIdx.bind(this)
   }
 
-  setTabIdx(tabIdx) {
-    this.setState({tabIdx})
-  }
+  setTabIdx = tabIdx => this.setState({ tabIdx })
 
   render() {
     return (
-      <Paper>
+      <div>
         <ModelToolBar />
         <Tabs
-          id='current-model-tabs'
-          value={this.state.tabIdx}
+          id="current-model-tabs"
+          index={this.state.tabIdx}
+          onChange={this.setTabIdx}
         >
-          <Tab
-            label='Fields'
-            value={0}
-            onClick={() => this.setTabIdx(0)}
-          >
+          <Tab label="Fields">
             <Fields />
           </Tab>
-          <Tab
-            label='Configuration'
-            value={1}
-            onClick={() => this.setTabIdx(1)}
-          >
+          <Tab label="Configuration">
             <Configuration />
           </Tab>
-          <Tab
-            label='Associations'
-            value={2}
-            onClick={() => this.setTabIdx(2)}
-          >
+          <Tab label="Associations">
             <Associations />
           </Tab>
         </Tabs>
-      </Paper>
+      </div>
     )
   }
 }
 
-
 const mapStateToProps = ({ models }) => ({ models })
 const mapDispatchToProps = dispatch => ({})
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(CurrentModel)
+export default connect(mapStateToProps, mapDispatchToProps)(CurrentModel)
