c522033b184f5c0e69d6c456b7d362f07b48ce87
Tom Schuster <tomjschuster@gmail.com>
2018-02-13 10:03:42 -0500

    only store models in local storage

--- src/store.js
+++ src/store.js
@@ -1,17 +1,19 @@
 import { compose, createStore, applyMiddleware } from 'redux'
 import logger from 'redux-logger'
 import adapter from 'redux-localstorage/lib/adapters/localStorage'
+import filter from 'redux-localstorage-filter'
 import persistState, { mergePersistedState } from 'redux-localstorage'
 import thunk from 'redux-thunk'
 
 import rootReducer from './redux'
 
+const storage = compose(filter('models'))(adapter(window.localStorage))
 const reducer = compose(mergePersistedState())(rootReducer)
 
 const enhancer = compose(
-  // applyMiddleware(logger, thunk),
-  applyMiddleware(thunk),
-  persistState(adapter(window.localStorage), 'models')
+  applyMiddleware(logger, thunk),
+  // applyMiddleware(thunk),
+  persistState(storage, 'sequelize-ui')
 )
 
 export default createStore(reducer, enhancer)
