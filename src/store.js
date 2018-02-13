40a963fd74485533f0729390e9579281a7ae2762
Tom Schuster <tomjschuster@gmail.com>
2018-02-13 10:05:13 -0500

    cleanup

--- src/store.js
+++ src/store.js
@@ -1,19 +1,21 @@
 import { compose, createStore, applyMiddleware } from 'redux'
 import logger from 'redux-logger'
+import thunk from 'redux-thunk'
+import persistState, { mergePersistedState } from 'redux-localstorage'
 import adapter from 'redux-localstorage/lib/adapters/localStorage'
 import filter from 'redux-localstorage-filter'
-import persistState, { mergePersistedState } from 'redux-localstorage'
-import thunk from 'redux-thunk'
 
 import rootReducer from './redux'
 
 const storage = compose(filter('models'))(adapter(window.localStorage))
-const reducer = compose(mergePersistedState())(rootReducer)
 
 const enhancer = compose(
   applyMiddleware(logger, thunk),
-  // applyMiddleware(thunk),
   persistState(storage, 'sequelize-ui')
 )
 
-export default createStore(reducer, enhancer)
+const reducer = compose(mergePersistedState())(rootReducer)
+
+const store = createStore(reducer, enhancer)
+
+export default store
