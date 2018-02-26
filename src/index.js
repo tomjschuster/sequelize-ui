ceb1c901247ab7c98a28d12104f2d974edf283da
Tom Schuster <tomjschuster@gmail.com>
2018-02-26 00:52:00 -0500

    load stylesheet in js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { ThemeProvider } from 'react-css-themr'
 import theme from './theme'
 import store from './store'
 import Routes from './Routes'
+import css from '../assets/main.css'
 
 render(
   <ThemeProvider theme={theme}>
