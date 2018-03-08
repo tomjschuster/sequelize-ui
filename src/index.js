39665fbddfff58d2fe1377d8482225d6e5f06507
Tom Schuster <tomjschuster@gmail.com>
2018-03-08 02:02:19 -0500

    just use standard with babel-esling

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ import { ThemeProvider } from 'react-css-themr'
 import theme from './theme'
 import store from './store'
 import Routes from './Routes'
+/* eslint-disable no-unused-vars */
 import css from './theme/css/main.css'
+/* eslint-enable no-unused-vars */
 
 render(
   <ThemeProvider theme={theme}>
