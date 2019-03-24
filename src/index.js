0790d6a71c80049fa0e822a835616495f69d1af3
Tom Schuster <tomjschuster@gmail.com>
2019-03-24 10:56:38 -0400

    mount app into body

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import { render } from 'react-dom'
 import App from './react/App.jsx'
 import './index.css'
 
-render(createElement(App), document.getElementById('app'))
+render(createElement(App), document.body)
