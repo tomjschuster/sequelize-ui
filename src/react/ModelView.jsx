43797455ce640c6fa8be171af301f065fd2fd0c4
Tom Schuster <tomjschuster@gmail.com>
2019-05-03 00:37:23 -0400

    model code styling

--- src/react/ModelView.jsx
+++ src/react/ModelView.jsx
@@ -30,63 +30,68 @@ export default class ModelView extends React.Component {
 
   render () {
     return (
-      <main className='main-content model-view'>
-        <h2 className='title'>{this.props.model.name} Model</h2>
-        <ToolBelt>
-          <Button icon='back' label='Back' onClick={this.props.goToModels} />
-          <Button icon='code' label='Code' onClick={this.toggleCode} />
+      <React.Fragment>
+        <main className='main-content model-view'>
           <Button
-            ref={this.editButtonRef}
-            icon='edit'
-            label='Edit'
-            onClick={this.props.editModel}
+            className='model-code__open'
+            icon='code'
+            label='Code'
+            onClick={this.toggleCode}
           />
-        </ToolBelt>
-        {this.state.codeOpen ? (
-          <div className='model-code'>
-            <Code
-              code={this.renderCode()}
-              copyButton
-              language='javascript'
-              onHide={this.toggleCode}
+          <h2 className='title'>{this.props.model.name} Model</h2>
+          <ToolBelt>
+            <Button icon='back' label='Back' onClick={this.props.goToModels} />
+            <Button
+              ref={this.editButtonRef}
+              icon='edit'
+              label='Edit'
+              onClick={this.props.editModel}
             />
-          </div>
-        ) : null}
-        <h3 className='fields-title subtitle'>Fields</h3>
-        {this.props.model.fields.length === 0 ? (
-          <p>No Fields</p>
-        ) : (
-          <table className='fields-table' key='abc'>
-            <thead>
-              <tr>
-                <th className='fields-table__name-header fields-table__cell'>
-                  Name
-                </th>
-                <th className='fields-table__cell'>Type</th>
-                <th className='fields-table__cell'>Options</th>
-              </tr>
-            </thead>
-            <tbody>
-              {this.props.model.fields.map(field => (
-                <tr key={field.id}>
-                  <td className='fields-table__name-cell fields-table__cell'>
-                    {field.name}
-                  </td>
-                  <td className='fields-table__cell'>
-                    {DATA_TYPE_OPTIONS[field.type]}
-                  </td>
-                  <td className='fields-table__cell'>
-                    {showFieldOptions(field)}
-                  </td>
+          </ToolBelt>
+          <h3 className='fields-title subtitle'>Fields</h3>
+          {this.props.model.fields.length === 0 ? (
+            <p>No Fields</p>
+          ) : (
+            <table className='fields-table' key='abc'>
+              <thead>
+                <tr>
+                  <th className='fields-table__name-header fields-table__cell'>
+                    Name
+                  </th>
+                  <th className='fields-table__cell'>Type</th>
+                  <th className='fields-table__cell'>Options</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
-        )}
-      </main>
+              </thead>
+              <tbody>
+                {this.props.model.fields.map(field => (
+                  <tr key={field.id}>
+                    <td className='fields-table__name-cell fields-table__cell'>
+                      {field.name}
+                    </td>
+                    <td className='fields-table__cell'>
+                      {DATA_TYPE_OPTIONS[field.type]}
+                    </td>
+                    <td className='fields-table__cell'>
+                      {showFieldOptions(field)}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          )}
+        </main>
+        <aside className={codeClass(this.state.codeOpen)}>
+          <div className='model-code__top'>
+            <Button label='Close' icon='cancel' onClick={this.toggleCode} />
+          </div>
+          <Code code={this.renderCode()} copyButton language='javascript' />
+        </aside>
+      </React.Fragment>
     )
   }
 }
+
+const codeClass = open => (open ? 'model-code open' : 'model-code')
 
 const showFieldOptions = field => {
   const options = {
