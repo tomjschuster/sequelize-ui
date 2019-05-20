81c50d880f08c037e01751064eac91514cb1157d
Tom Schuster <tomjschuster@gmail.com>
2019-05-20 03:13:17 -0400

    polyfills

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,10 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
+            presets: [
+              ['@babel/preset-env', { useBuiltIns: 'entry', corejs: 3 }],
+              '@babel/preset-react'
+            ],
             plugins: ['@babel/plugin-proposal-class-properties']
           }
         }
