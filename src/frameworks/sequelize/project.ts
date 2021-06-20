aa21cf0bd27448ee8df4fbdc23a9baf7f0dd6579
Tom Schuster <tomjschuster@gmail.com>
2021-06-19 23:01:04 -0400

    move sequelize project utils to helpers

--- src/frameworks/sequelize/project.ts
+++ src/frameworks/sequelize/project.ts
@@ -1,19 +1,7 @@
 import { DbOptions } from '@src/core/database'
 import { directory, DirectoryItem, file } from '@src/core/files'
-import {
-  Association,
-  AssociationTypeType,
-  belongsToType,
-  Field,
-  Model,
-  Schema,
-  ThroughType,
-  typeWithoutOptions,
-} from '@src/core/schema'
-import { arrayToLookup, dedupBy } from '@src/utils/array'
-import { addSeconds, now, toNumericTimestamp } from '@src/utils/dateTime'
-import { kebabCase, namesEqSingular, noCase, normalize } from '@src/utils/string'
-import shortid from 'shortid'
+import { Schema } from '@src/core/schema'
+import { kebabCase } from '@src/utils/string'
 import { config } from './templates/config'
 import { dbTemplate } from './templates/db'
 import { gitignoreTemplate } from './templates/gitignore'
@@ -26,14 +14,15 @@ import { serverTemplate } from './templates/server'
 import { tsconfigTemplate } from './templates/tsconfig'
 import { typesTemplate } from './templates/types'
 import {
-  getFieldsWithPk,
-  getForeignKey,
-  getOtherKey,
+  dedupModels,
+  getJoinTables,
   hasJsonType,
-  idField,
   migrationCreateFilename,
   migrationForeignKeysFilename,
+  migrationTimestamp,
   modelName,
+  nextTimestamp,
+  normalizeSchema,
 } from './utils/helpers'
 
 type GenerateSequelizeProjectArgs = {
@@ -86,156 +75,4 @@ export function generateSequelizeProject({
     schema.models.some(hasJsonType) ? file('types.ts', typesTemplate()) : null,
     file('tsconfig.json', tsconfigTemplate()),
   ])
-}
-
-type NormalizeSchemaArgs = {
-  schema: Schema
-  dbOptions: DbOptions
-}
-const normalizeSchema = ({ schema, dbOptions }: NormalizeSchemaArgs): Schema => {
-  return { ...schema, models: schema.models.map((model) => normalizeModel({ model, dbOptions })) }
-}
-
-type NormalizeModelArgs = {
-  model: Model
-  dbOptions: DbOptions
-}
-const normalizeModel = ({ model, dbOptions }: NormalizeModelArgs): Model => {
-  return { ...model, fields: getFieldsWithPk({ model, dbOptions }) }
-}
-
-function dedupModels(models: Model[]): Model[] {
-  const visitedModels: Record<string, Model> = {}
-  const names: string[] = []
-  models.forEach((model) => {
-    const name = noCase(model.name)
-    const visitedModel = visitedModels[name]
-    if (visitedModel) {
-      visitedModels[name] = mergeFields(visitedModel, model)
-    } else {
-      visitedModels[name] = model
-      names.push(name)
-    }
-  })
-
-  return names.map((name) => visitedModels[name])
-}
-
-function mergeFields(x: Model, y: Model): Model {
-  const xHasPk = x.fields.some((f) => f.primaryKey)
-
-  const [, yDiffs] = y.fields.reduce(
-    (acc, y_) => {
-      const { primaryKey: _, ...yWithoutPk } = y_
-      const y = xHasPk ? yWithoutPk : y_
-      if (x.fields.some((x) => namesEqSingular(x.name, y.name))) {
-        acc[0].push(y)
-      } else {
-        acc[1].push(y)
-      }
-      return acc
-    },
-    [[], []] as [Field[], Field[]],
-  )
-
-  const fields = dedupBy([...x.fields, ...yDiffs], (f) => normalize(f.name))
-  return { ...x, fields }
-}
-
-function getJoinTables(schema: Schema, dbOptions: DbOptions): Model[] {
-  const modelById = arrayToLookup(schema.models, (m) => m.id)
-  return schema.models
-    .flatMap((m) => m.associations.map((a) => getJoinTableModel(a, modelById, dbOptions)))
-    .filter((m): m is Model => !!m)
-}
-
-function getJoinTableModel(
-  association: Association,
-  modelById: Map<string, Model>,
-  dbOptions: DbOptions,
-): Model | null {
-  if (association.type.type !== AssociationTypeType.ManyToMany) return null
-
-  const id = shortid()
-
-  const tableName =
-    association.type.through.type === ThroughType.ThroughTable
-      ? association.type.through.table
-      : null
-
-  if (!tableName) return null
-
-  const source = modelById.get(association.sourceModelId)
-  const target = modelById.get(association.targetModelId)
-
-  if (!source || !target) return null
-
-  const sourceFk = getOtherKey({ association, modelById, dbOptions })
-
-  if (!sourceFk) return null
-
-  const sourcePk =
-    source.fields.find((field) => field.primaryKey) || idField({ model: source, dbOptions })
-
-  const sourceFkField = {
-    id: shortid(),
-    name: sourceFk,
-    type: typeWithoutOptions(sourcePk.type),
-    primaryKey: true,
-  }
-
-  const sourceAssoc: Association = {
-    id: shortid(),
-    sourceModelId: id,
-    targetModelId: source.id,
-    type: belongsToType(),
-  }
-
-  const targetFk = getForeignKey({ model: target, association, modelById, dbOptions })
-
-  const targetPk =
-    target.fields.find((field) => field.primaryKey) || idField({ model: target, dbOptions })
-
-  const targetFkField = {
-    id: shortid(),
-    name: targetFk,
-    type: typeWithoutOptions(targetPk.type),
-    primaryKey: true,
-  }
-
-  const targetAssoc: Association = {
-    id: shortid(),
-    sourceModelId: id,
-    targetModelId: target.id,
-    type: belongsToType(),
-  }
-
-  return {
-    id,
-    name: tableName,
-    createdAt: source.createdAt,
-    updatedAt: source.updatedAt,
-    fields: [sourceFkField, targetFkField],
-    associations: [sourceAssoc, targetAssoc],
-  }
-}
-
-type MigrationTimestamps = Map<number, Model>
-function migrationTimestamp(timestamps: MigrationTimestamps, model: Model): MigrationTimestamps {
-  const timestamp = toNumericTimestamp(model.createdAt)
-  if (timestamps.get(timestamp)) return migrationTimestamp(timestamps, add10(model))
-  return timestamps.set(timestamp, model)
-}
-
-function add10(model: Model): Model {
-  return {
-    ...model,
-    createdAt: addSeconds(model.createdAt, 10),
-    updatedAt: addSeconds(model.updatedAt, 10),
-  }
-}
-
-function nextTimestamp(timestamps: MigrationTimestamps): number {
-  const currMax = Math.max(0, ...timestamps.keys())
-  return currMax ? currMax + 10 : toNumericTimestamp(now())
 }
--- src/frameworks/sequelize/utils/helpers.ts
+++ src/frameworks/sequelize/utils/helpers.ts
@@ -9,6 +9,7 @@ import {
   Association,
   associationTypeIsSingular,
   AssociationTypeType,
+  belongsToType,
   DataTypeType,
   dateTimeDataType,
   Field,
@@ -19,7 +20,18 @@ import {
   typeWithoutOptions,
 } from '@src/core/schema'
 import { arrayToLookup, dedupBy } from '@src/utils/array'
-import { camelCase, namesEq, pascalCase, plural, singular, snakeCase } from '@src/utils/string'
+import { addSeconds, now, toNumericTimestamp } from '@src/utils/dateTime'
+import {
+  camelCase,
+  namesEq,
+  namesEqSingular,
+  noCase,
+  normalize,
+  pascalCase,
+  plural,
+  singular,
+  snakeCase,
+} from '@src/utils/string'
 import shortid from 'shortid'
 
 export function modelName({ name }: Model): string {
@@ -356,4 +368,159 @@ export function getOtherKey({ association, modelById, dbOptions }: GetOtherKeyAr
   const name = association.alias ? association.alias : target.name
 
   return caseByDbCaseStyle(`${name} id`, dbOptions.caseStyle)
+}
+
+type NormalizeSchemaArgs = {
+  schema: Schema
+  dbOptions: DbOptions
+}
+export const normalizeSchema = ({ schema, dbOptions }: NormalizeSchemaArgs): Schema => {
+  return { ...schema, models: schema.models.map((model) => normalizeModel({ model, dbOptions })) }
+}
+
+type NormalizeModelArgs = {
+  model: Model
+  dbOptions: DbOptions
+}
+const normalizeModel = ({ model, dbOptions }: NormalizeModelArgs): Model => {
+  return { ...model, fields: getFieldsWithPk({ model, dbOptions }) }
+}
+
+export function dedupModels(models: Model[]): Model[] {
+  const visitedModels: Record<string, Model> = {}
+  const names: string[] = []
+  models.forEach((model) => {
+    const name = noCase(model.name)
+    const visitedModel = visitedModels[name]
+    if (visitedModel) {
+      visitedModels[name] = mergeFields(visitedModel, model)
+    } else {
+      visitedModels[name] = model
+      names.push(name)
+    }
+  })
+
+  return names.map((name) => visitedModels[name])
+}
+
+function mergeFields(x: Model, y: Model): Model {
+  const xHasPk = x.fields.some((f) => f.primaryKey)
+
+  const [, yDiffs] = y.fields.reduce(
+    (acc, y_) => {
+      const { primaryKey: _, ...yWithoutPk } = y_
+      const y = xHasPk ? yWithoutPk : y_
+      if (x.fields.some((x) => namesEqSingular(x.name, y.name))) {
+        acc[0].push(y)
+      } else {
+        acc[1].push(y)
+      }
+      return acc
+    },
+    [[], []] as [Field[], Field[]],
+  )
+
+  const fields = dedupBy([...x.fields, ...yDiffs], (f) => normalize(f.name))
+  return { ...x, fields }
+}
+
+export function getJoinTables(schema: Schema, dbOptions: DbOptions): Model[] {
+  const modelById = arrayToLookup(schema.models, (m) => m.id)
+  return schema.models
+    .flatMap((m) => m.associations.map((a) => getJoinTableModel(a, modelById, dbOptions)))
+    .filter((m): m is Model => !!m)
+}
+
+function getJoinTableModel(
+  association: Association,
+  modelById: Map<string, Model>,
+  dbOptions: DbOptions,
+): Model | null {
+  if (association.type.type !== AssociationTypeType.ManyToMany) return null
+
+  const id = shortid()
+
+  const tableName =
+    association.type.through.type === ThroughType.ThroughTable
+      ? association.type.through.table
+      : null
+
+  if (!tableName) return null
+
+  const source = modelById.get(association.sourceModelId)
+  const target = modelById.get(association.targetModelId)
+
+  if (!source || !target) return null
+
+  const sourceFk = getOtherKey({ association, modelById, dbOptions })
+
+  if (!sourceFk) return null
+
+  const sourcePk =
+    source.fields.find((field) => field.primaryKey) || idField({ model: source, dbOptions })
+
+  const sourceFkField = {
+    id: shortid(),
+    name: sourceFk,
+    type: typeWithoutOptions(sourcePk.type),
+    primaryKey: true,
+  }
+
+  const sourceAssoc: Association = {
+    id: shortid(),
+    sourceModelId: id,
+    targetModelId: source.id,
+    type: belongsToType(),
+  }
+
+  const targetFk = getForeignKey({ model: target, association, modelById, dbOptions })
+
+  const targetPk =
+    target.fields.find((field) => field.primaryKey) || idField({ model: target, dbOptions })
+
+  const targetFkField = {
+    id: shortid(),
+    name: targetFk,
+    type: typeWithoutOptions(targetPk.type),
+    primaryKey: true,
+  }
+
+  const targetAssoc: Association = {
+    id: shortid(),
+    sourceModelId: id,
+    targetModelId: target.id,
+    type: belongsToType(),
+  }
+
+  return {
+    id,
+    name: tableName,
+    createdAt: source.createdAt,
+    updatedAt: source.updatedAt,
+    fields: [sourceFkField, targetFkField],
+    associations: [sourceAssoc, targetAssoc],
+  }
+}
+
+type MigrationTimestamps = Map<number, Model>
+export function migrationTimestamp(
+  timestamps: MigrationTimestamps,
+  model: Model,
+): MigrationTimestamps {
+  const timestamp = toNumericTimestamp(model.createdAt)
+  if (timestamps.get(timestamp)) return migrationTimestamp(timestamps, add10(model))
+  return timestamps.set(timestamp, model)
+}
+
+function add10(model: Model): Model {
+  return {
+    ...model,
+    createdAt: addSeconds(model.createdAt, 10),
+    updatedAt: addSeconds(model.updatedAt, 10),
+  }
+}
+
+export function nextTimestamp(timestamps: MigrationTimestamps): number {
+  const currMax = Math.max(0, ...timestamps.keys())
+  return currMax ? currMax + 10 : toNumericTimestamp(now())
 }
