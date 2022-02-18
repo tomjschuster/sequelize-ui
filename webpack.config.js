bc6c5ed7be3adf2c2d37b5ff3d0b4565a39856d0
Tom Schuster <tomjschuster@gmail.com>
2022-02-18 00:33:49 -0500

    build to out

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,37 @@
-const path = require('path')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const CopyPlugin = require('copy-webpack-plugin')
-const RobotstxtPlugin = require('robotstxt-webpack-plugin')
-const SitemapPlugin = require('sitemap-webpack-plugin').default
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const RobotstxtPlugin = require("robotstxt-webpack-plugin");
+const SitemapPlugin = require("sitemap-webpack-plugin").default;
 
-const production = process.env.NODE_ENV === 'production'
+const production = process.env.NODE_ENV === "production";
 
-const alias = production ? {} : { 'react-dom': '@hot-loader/react-dom' }
+const alias = production ? {} : { "react-dom": "@hot-loader/react-dom" };
 
 module.exports = {
-  entry: ['core-js/features/array/flat-map', './src/index.js'],
+  entry: ["core-js/features/array/flat-map", "./src/index.js"],
   output: {
-    path: path.resolve(__dirname, 'dist/'),
-    filename: 'static/[name].[fullhash].js'
+    path: path.resolve(__dirname, "out/"),
+    filename: "static/[name].[fullhash].js",
   },
-  resolve: { extensions: ['.js', '.jsx'], alias },
-  devtool: production ? 'source-map' : 'eval-source-map',
+  resolve: { extensions: [".js", ".jsx"], alias },
+  devtool: production ? "source-map" : "eval-source-map",
   module: {
     rules: [
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
             presets: [
-              ['@babel/preset-env', { useBuiltIns: 'entry', corejs: 3 }],
-              '@babel/preset-react'
+              ["@babel/preset-env", { useBuiltIns: "entry", corejs: 3 }],
+              "@babel/preset-react",
             ],
-            plugins: ['@babel/plugin-proposal-class-properties']
-          }
-        }
+            plugins: ["@babel/plugin-proposal-class-properties"],
+          },
+        },
       },
       {
         test: /\.css$/,
@@ -40,73 +40,73 @@ module.exports = {
           {
             loader: MiniCssExtractPlugin.loader,
             options: {
-              publicPath: '../'
-            }
+              publicPath: "../",
+            },
           },
           {
-            loader: 'css-loader',
-            options: { importLoaders: 1 }
+            loader: "css-loader",
+            options: { importLoaders: 1 },
           },
           {
-            loader: 'postcss-loader',
+            loader: "postcss-loader",
             options: {
               postcssOptions: {
-                ident: 'postcss',
-                plugins: ['autoprefixer']
-              }
-            }
-          }
-        ]
+                ident: "postcss",
+                plugins: ["autoprefixer"],
+              },
+            },
+          },
+        ],
       },
       {
         test: /\.jpe?g$|\.gif$|\.ico$|\.png$|\.svg$/,
-        loader: 'file-loader',
+        loader: "file-loader",
         options: {
-          name: '[name].[ext]',
-          outputPath: 'static/images'
-        }
+          name: "[name].[ext]",
+          outputPath: "static/images",
+        },
       },
       {
         test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'url-loader',
+        loader: "url-loader",
         options: {
           limit: 10000,
-          mimetype: 'application/font-woff',
-          name: '[name].[ext]',
-          outputPath: 'static/fonts'
-        }
+          mimetype: "application/font-woff",
+          name: "[name].[ext]",
+          outputPath: "static/fonts",
+        },
       },
       {
         test: /\.(ttf|eot)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'file-loader',
+        loader: "file-loader",
         options: {
-          name: '[name].[ext]',
-          outputPath: 'static/fonts'
-        }
+          name: "[name].[ext]",
+          outputPath: "static/fonts",
+        },
       },
       {
         test: /\.otf(\?.*)?$/,
-        loader: 'file-loader',
+        loader: "file-loader",
         options: {
-          name: '/fonts/[name].[ext]',
-          mimetype: 'application/font-otf',
-          outputPath: 'static'
-        }
-      }
-    ]
+          name: "/fonts/[name].[ext]",
+          mimetype: "application/font-otf",
+          outputPath: "static",
+        },
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'src/index.html',
-      filename: 'index.html'
+      template: "src/index.html",
+      filename: "index.html",
     }),
-    new MiniCssExtractPlugin({ filename: 'static/[name].[fullhash].css' }),
-    new CopyPlugin({ patterns: [{ from: 'assets', to: 'static' }] }),
+    new MiniCssExtractPlugin({ filename: "static/[name].[fullhash].css" }),
+    new CopyPlugin({ patterns: [{ from: "assets", to: "static" }] }),
     new RobotstxtPlugin(),
     new SitemapPlugin({
-      base: 'https://sequelizeui.app',
-      paths: ['/'],
-      options:{ lastMod: true }
-    })
-  ]
-}
+      base: "https://sequelizeui.app",
+      paths: ["/"],
+      options: { lastMod: true },
+    }),
+  ],
+};
