15d18512435c2fa59f79d2c8b29e0ec2e8ea9cae
Tom Schuster <tomjschuster@gmail.com>
2022-02-17 23:01:25 -0500

    delete

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './react/App.jsx'
-import './style/index.css'
-
-ReactDOM.render(React.createElement(App), document.getElementById('app'))
