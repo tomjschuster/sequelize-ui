c86c878b5ca31ee5a51345e85127f2d776067218
Tom Schuster <tomjschuster@gmail.com>
2022-02-17 22:01:29 -0500

    favicons

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -22,6 +22,11 @@ class SequelizeUiDocument extends Document {
       <Html lang="en">
         <Head>
           <script dangerouslySetInnerHTML={{ __html: syncDomDarkModeScriptSource() }} />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="icon" href="/favicon.ico" />
+          <link rel="manifest" href="/site.webmanifest" />
         </Head>
         <body>
           <Main />
