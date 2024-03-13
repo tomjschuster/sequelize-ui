dfc275f7d15fbb47452980a0cc37a41e86758e85
Tom Schuster <tomjschuster@gmail.com>
2024-03-12 23:28:14 -0400

    Use target model for otherKey in N:N table assoc

--- e2e/migrations/schemaCases/index.ts
+++ e2e/migrations/schemaCases/index.ts
@@ -2,7 +2,13 @@ import { ExpectedSchemaCase } from '../cases'
 import { default as blogCases } from './blog'
 import { default as employeesCases } from './employees'
 import { default as sakilaCases } from './sakila'
+import { default as studentInformationSystemCases } from './studentInformationSystem'
 
-const cases: ExpectedSchemaCase[] = [blogCases, employeesCases, sakilaCases]
+const cases: ExpectedSchemaCase[] = [
+  blogCases,
+  employeesCases,
+  sakilaCases,
+  studentInformationSystemCases,
+]
 
 export default cases
